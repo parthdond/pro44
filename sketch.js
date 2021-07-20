7dd09a87b0ebe6a2f47bcac1a3a36bfd829cb630
parthdond@gmail.com <parthdond2006>
2021-07-20 12:10:24 +0530

    button added

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ var bg,bg2,form,system,code,security;
 var score=0;
 
 function preload() {
-  bg = loadImage("aladdin_cave.jpg");
+  bg = loadImage("Images/aladdin_cave.jpg");
+  bg2 = loadImage("Images/treasure.jpg")
 }
 
 function setup() {
@@ -17,10 +18,18 @@ function draw() {
   security.display();
 
   // Add code to display score in the middle of the screen
+  textSize(20);
+  text("SCORE : "+score,500,100);
 
 
   // Add code to display the end screen
-
+if(score ===3){
+  clear()
+  background(bg2);
+  fill("black")
+  textSize(40);
+  text("TRESURE UNLOCKED",250,200)
+}
 
   drawSprites()
 }
